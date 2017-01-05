464db3b4246cdf232f6c3e32676121d2419a0d31
pflarcology <rhrg500@gmail.com>
2017-01-05 09:52:08 +0000

    working mock

--- Spec/note-controller-test.js
+++ Spec/note-controller-test.js
@@ -5,18 +5,15 @@ function testNoteControllerCanBeInstantiated(){
 
 function testInnerHtml(){
   function NoteListViewDouble() {};
-  // NoteListViewDouble.prototype = {
-  //   returnsHtmlist: function() {
-  //     return "<ul><li><div>Favourite food: pesto</div></li></ul>"
-  //   }
-  // };
+  NoteListViewDouble.prototype = {
+    returnsHtmlList: function() {
+      return "<ul><li><div>Favourite food: pesto</div></li></ul>"
+    }
+  };
   var noteController = new NoteController();
   var noteListViewDouble = new NoteListViewDouble();
-  noteController.createNote("Favourite food: pesto");
-  var noteListModel = new NoteList();
-  var html = document.getElementById("app");
+  noteController.noteListView = noteListViewDouble;
   noteController.getHtml();
-  var noteListView = noteListViewDouble;
   element = document.getElementById('app')
   assert.isTrue(element.innerHTML === "<ul><li><div>Favourite food: pesto</div></li></ul>", "testInnerHtml")
 };
