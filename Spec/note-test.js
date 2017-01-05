dfb6945f6d56648997c1c48c02c935717624a955
ajbater <amanda22@hotmail.co.uk>
2017-01-05 13:49:40 +0000

    note is instantiated with a unique id

--- Spec/note-test.js
+++ Spec/note-test.js
@@ -8,6 +8,13 @@ function testNoteTextCanBeSeen() {
   assert.isTrue(note.showNoteText() === "a string", "testNoteTextCanBeSeen");
 };
 
+function testNoteHasAUniqueID() {
+  var note = new Note("text");
+  var secondNote = new Note("text");
+  assert.isTrue(note.id === secondNote.id - 1, "testNoteHasAUniqueID");
+};
+
 
 testNoteTakesTextUponInstantiation();
 testNoteTextCanBeSeen();
+testNoteHasAUniqueID();
--- Src/note-model.js
+++ Src/note-model.js
@@ -1,6 +1,10 @@
 (function(exports) {
+
+  var idCounter = 0;
+
   function Note(text) {
     this.noteText = text;
+    this.id = idCounter++;
   };
   Note.prototype.showNoteText = function(){
     return this.noteText;
