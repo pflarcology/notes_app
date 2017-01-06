7a67f1d6898e2e148dbbcf0b3c4af036eef8015c
Russell Yeo <rusyeo@gmail.com>
2017-01-06 14:07:48 +0000

    text input for adding notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-noteList = new NoteList();
-noteListView = new NoteListView(noteList);
-noteController = new NoteController(noteList, noteListView);
-noteController.createNote("im pairing with russel today and it is great");
-noteController.getHtml(this.noteListView.returnsHtmlList());
-window.addEventListener("hashchange", function() { noteController.updateSingleNote()});
+window.onload = function() {
+  noteList = new NoteList();
+  noteListView = new NoteListView(noteList);
+  noteController = new NoteController(noteList, noteListView);
+  noteController.createNote("im pairing with russel today and it is great");
+  noteController.getHtml(this.noteListView.returnsHtmlList());
+  window.addEventListener("hashchange", function() { noteController.updateSingleNote()});
+}
